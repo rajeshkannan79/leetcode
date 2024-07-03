6137a0fe35e40fa3aabe4c1eda0e03bb1068df23
Rajeshkannan T <167198213+rajeshkannan79@users.noreply.github.com>
2024-07-03 11:51:29 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (96.29%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -2,18 +2,18 @@ class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
        int changecolor=image[sr][sc]; 
         if(changecolor!=color){
-            madesh(image,sr,sc,changecolor,color);
+            m(image,sr,sc,changecolor,color);
         }
         return image;
     }
-    public static void madesh(int[][] image,int sr,int sc,int changecolor,int color){
+    public static void m(int[][] image,int sr,int sc,int changecolor,int color){
         if(sr<0 ||  sr>=image.length || sc<0 || sc>=image[0].length || image[sr][sc]!=changecolor){
             return;
         }
         image[sr][sc]=color;
-        madesh(image,sr-1,sc,changecolor,color);
-        madesh(image,sr+1,sc,changecolor,color);
-        madesh(image,sr,sc-1,changecolor,color);
-        madesh(image,sr,sc+1,changecolor,color);
+        m(image,sr-1,sc,changecolor,color);
+        m(image,sr+1,sc,changecolor,color);
+        m(image,sr,sc-1,changecolor,color);
+        m(image,sr,sc+1,changecolor,color);
     }
 }
