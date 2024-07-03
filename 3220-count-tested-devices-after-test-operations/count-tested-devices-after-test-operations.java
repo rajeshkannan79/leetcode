90cc8e63ea606dd6180ab63cd3fe029477cde600
Rajeshkannan T <167198213+rajeshkannan79@users.noreply.github.com>
2024-07-03 13:03:24 +0530

    Time: 1 ms (65.31%) | Memory: 42.8 MB (87.80%) - LeetSync

---
+++ 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countTestedDevices(int[] b) {
+        int c=0;
+        for(int i=0;i<b.length;i++)
+        {
+            if(b[i]>0)
+            {
+                c++;
+                for(int j=i+1;j<b.length;j++)
+                {
+                    b[j]--;
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
+       return c;
+    }
+}
