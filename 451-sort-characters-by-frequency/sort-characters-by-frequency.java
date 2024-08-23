0a1db95a1ddec2c9d01c8b0bba7360a122a205f4
Rajeshkannan T <167198213+rajeshkannan79@users.noreply.github.com>
2024-08-23 22:22:34 +0530

    Time: 16 ms (42.91%) | Memory: 45.5 MB (63.48%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,39 +1,24 @@
+import java.util.*;
+
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character,Integer> hm=new HashMap<>();
-        for(int i=0;i<s.length();i++)
-        {
-            if(hm.containsKey(s.charAt(i)))
-            {
-                hm.put(s.charAt(i),hm.get(s.charAt(i))+1);
-            }
-            else
-            {
-                hm.put(s.charAt(i),1);
-            }
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
         }
-       String sb="";
-       
-        while(!hm.isEmpty())
-        {
-             int max=0;
-        char m=' ';
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(hm.entrySet());
 
-           for(Map.Entry<Character,Integer> e:hm.entrySet())
-           {
-               int h=e.getValue();
-              if(max<h)
-              {
-                max=h;
-                m=e.getKey();
-              }
-           } 
-           for(int i=0;i<max;i++)
-           {
-            sb=sb+m;
-           }
-           hm.remove(m);
+       
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Character, Integer> entry : list) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                sb.append(entry.getKey());
+            }
         }
-        return sb;
+
+        return sb.toString();
     }
 }
