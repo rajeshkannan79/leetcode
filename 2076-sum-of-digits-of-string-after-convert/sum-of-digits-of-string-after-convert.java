aab23e7eead640103ad2e4ee78fa35af04ecb372
Rajeshkannan T <167198213+rajeshkannan79@users.noreply.github.com>
2024-09-03 16:02:26 +0530

    Time: 3 ms (37.62%) | Memory: 42.4 MB (35.50%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -1,25 +1,32 @@
 class Solution {
     public int getLucky(String s, int k) {
+      
+        HashMap<Character, Integer> hm = new HashMap<>();
+        char c = 'a';
+        for (int i = 1; i <= 26; i++) {
+            hm.put(c++, i);
+        }
+        
+        // Use StringBuilder to build the number representation
         StringBuilder sb = new StringBuilder();
-        int sum = 0;
-        int n = s.length();
-        for(int i=0;i<n;i++){
-            sb.append(((int)(s.charAt(i)) - 96));
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(hm.get(s.charAt(i)));
         }
-        for(int i=0;i<sb.length();i++){
-            sum += ((sb.charAt(i))-'0');
-        }
-        k--;
-        while(k!=0){
-            int num = sum;
-            sum = 0;
-            while(num!=0){
-                int a = num%10;
-                num/=10;
-                sum += a;
+        
+        // Convert the concatenated number to a string for processing
+        String numberStr = sb.toString();
+        
+        // Perform the digit sum operation k times
+        while (k > 0) {
+            long sum = 0;
+            for (char ch : numberStr.toCharArray()) {
+                sum += ch - '0'; // Convert char digit to its integer value
             }
+            numberStr = Long.toString(sum); // Convert the sum back to string for next iteration
             k--;
         }
-        return sum;
+        
+        // Return the final single-digit result
+        return Integer.parseInt(numberStr);
     }
 }
