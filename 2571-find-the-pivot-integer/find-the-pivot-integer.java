ababdf39efeddda3e69204d166f03b800ced6f18
Rajeshkannan T <167198213+rajeshkannan79@users.noreply.github.com>
2024-12-21 14:42:46 +0530

    Time: 52 ms (5.37%) | Memory: 41.2 MB (10.64%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,25 +1,25 @@
 class Solution {
     public int pivotInteger(int n) {
-      ArrayList<Integer> al=new ArrayList<>();if(n==1)return 1;
-      for(int i=1;i<=n;i++)
-      {
-        al.add(i);
-      }
-      for(int i=1;i<al.size()-1;i++)
-      {int b=0;
-      int sum=0;
-       for(int j=0;j<i;j++)
+       int arr[]=new int[n+1];int o=0;
+       for(int i=1;i<=n;i++)
        {
-        sum=sum+al.get(j);
-       }
-       for(int k=i+1;k<al.size();k++)
-       {
-        b=b+al.get(k);
-       }
-       if(sum==b)
-       {
-        return al.get(i);
-       }
-      }return -1;
+        arr[o++]=i;
+       }if(n==1)return 1;
+       for(int i=1;i<arr.length;i++)
+       {int sum1=0;
+       int sum2=0;
+        for(int j=0;j<i;j++)
+        {
+            sum1+=arr[j];
+        }
+        for(int j1=i+1;j1<arr.length;j1++)
+        {
+            sum2+=arr[j1];
+        }
+        if(sum1==sum2)
+        {
+            return arr[i];
+        }
+       } return -1;
     }
 }
