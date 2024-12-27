9b96e12507c6122b57f75ee217e29402dcb5001d
Rajeshkannan T <167198213+rajeshkannan79@users.noreply.github.com>
2024-12-27 10:18:26 +0530

    Time: 1 ms (99.33%) | Memory: 41 MB (64.23%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.java
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.java
@@ -1,14 +1,14 @@
 class Solution {
     public int distinctAverages(int[] nums) {
-        ArrayList<Double> al=new ArrayList();
+       HashSet<Double> al=new HashSet();
         Arrays.sort(nums);int left=0;int right=nums.length-1;
         while(left<right)
         {
           Double mid=(nums[left]+nums[right])/2.0;
-          if(!al.contains(mid))
-          {System.out.print(mid+" ");
+        
+        
             al.add(mid);
-          }
+          
           left++;right--;
         }
         return al.size();
